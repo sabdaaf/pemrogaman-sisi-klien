67a51363e1f6a623a883478b056ca8dfb1eb5fb0
Fuad Ali Rahman <alirahmanfuad@gmail.com>
2025-05-19 15:53:10 +0700

    update basic.js

---
+++ basic_js/ES6/ES6.js
@@ -0,0 +1,8 @@
+// Destructuring Assignment
+// destructuring pada object
+const mataKuliahList = {
+  tahunAjar: 2024,
+  mataKuliah: ["matematika", "fisika", "kimia"],
+};
+const { tahunAjar, mataKuliah } = mataKuliahList;
+console.log(tahunAjar, mataKuliah);
