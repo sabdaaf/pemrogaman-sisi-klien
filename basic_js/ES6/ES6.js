4eb6c5549d0a0eb12eff36715a81f2606f16cfd8
Fuad Ali Rahman <alirahmanfuad@gmail.com>
2025-05-20 14:53:22 +0700

    add destructuring

--- basic_js/ES6/ES6.js
+++ basic_js/ES6/ES6.js
@@ -6,3 +6,21 @@ const mataKuliahList = {
 };
 const { tahunAjar, mataKuliah } = mataKuliahList;
 console.log(tahunAjar, mataKuliah);
+
+const [mk1, mk2] = mataKuliahList.mataKuliah;
+console.log(mk1, mk2);
+
+mataKuliahList.mataKuliah.push("biologi"); // tambah element baru diakhir array
+console.log(mataKuliahList.mataKuliah);
+
+mataKuliahList.mataKuliah.unshift("bahasa indonesia"); // tambah element baru di awal array
+console.log(mataKuliahList.mataKuliah);
+
+mataKuliahList.mataKuliah.pop(); // hapus element terakhir di array
+console.log(mataKuliahList.mataKuliah);
+
+mataKuliahList.mataKuliah.shift(); // hapus element pertama di array
+console.log(mataKuliahList.mataKuliah);
+
+mataKuliahList.mataKuliah = [...mataKuliahList.mataKuliah, "biologi", "sastra indo", "sastra inggris"]; // tambah element baru diakhir array
+console.log(mataKuliahList.mataKuliah);
